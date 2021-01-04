b79f416796ab29e9de6d892995af1c56f10e5026
rashad101 <rah.rony@gmail.com>
2021-01-04 12:40:41 +0600

    cleaned up the base repo

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Counter } from "./features/counter/Counter";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="app">
+      <h1>Gmail in construction</h1>
+    </div>
+  );
+}
+
+export default App;
