4d229b224760c0f5ba19d1ea4237f6c62e4113f4
rashad101 <rah.rony@gmail.com>
2021-01-13 12:22:32 +0600

    added gmail header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from "react";
-import { Counter } from "./features/counter/Counter";
 import "./App.css";
+import Header from "./Header";
 
 function App() {
   return (
     <div className="app">
-      <h1>Gmail in construction</h1>
+      <Header />
     </div>
   );
 }
---
+++ src/Header.js
@@ -0,0 +1,43 @@
+import React from "react";
+import "./Header.css";
+import MenuIcon from "@material-ui/icons/Menu";
+import { Avatar, IconButton } from "@material-ui/core";
+import SearchIcon from "@material-ui/icons/Search";
+import ArrowDropDownIcon from "@material-ui/icons/ArrowDropDown";
+import AppsIcon from "@material-ui/icons/Apps";
+import NotificationsIcon from "@material-ui/icons/Notifications";
+
+function Header() {
+  return (
+    <div className="header">
+      <div className="header__left">
+        <IconButton>
+          <MenuIcon />
+        </IconButton>
+        <img
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQzRceIIBz4GgeNszaN5SupI6p1SJE_Bzgk3Q&usqp=CAU"
+          alt="gmail logo"
+        />
+      </div>
+
+      <div className="header__middle">
+        <SearchIcon />
+        <input placeholder="Search mail" type="text" />
+        <ArrowDropDownIcon className="header__inputCaret" />
+      </div>
+
+      <div className="header__right">
+        <IconButton>
+          <AppsIcon />
+        </IconButton>
+        <IconButton>
+          <NotificationsIcon />
+        </IconButton>
+
+        <Avatar />
+      </div>
+    </div>
+  );
+}
+
+export default Header;
